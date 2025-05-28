961570834dfce8b6d22d09d2e3ee2c4daf33080b
Denzel Huijbers <huijbers15@outlook.com>
2025-05-28 12:32:39 +0200

    test update

--- pbuild-business/obj/Debug/net8.0/pbuild-business.AssemblyInfo.cs
+++ pbuild-business/obj/Debug/net8.0/pbuild-business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pbuild-business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c419bd99f1deb878de99e0465a8024233051baf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c977924698e2a50491fc7f5847e4e99588bf5123")]
 [assembly: System.Reflection.AssemblyProductAttribute("pbuild-business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pbuild-business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
