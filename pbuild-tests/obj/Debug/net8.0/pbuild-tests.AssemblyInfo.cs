99f44a958286f33d5064978865879a6937fba361
Denzel Huijbers <huijbers15@outlook.com>
2025-05-29 16:28:31 +0200

    d

--- pbuild-tests/obj/Debug/net8.0/pbuild-tests.AssemblyInfo.cs
+++ pbuild-tests/obj/Debug/net8.0/pbuild-tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pbuild-tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c977924698e2a50491fc7f5847e4e99588bf5123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b680f1c1823f0109bf354e2702a820773ba6b251")]
 [assembly: System.Reflection.AssemblyProductAttribute("pbuild-tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pbuild-tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
